8b75f9128b77bd81e2f11ee371ec0d3d273e8bce
Kevin <kevin.kiseli@student.moringaschool.com>
2023-11-03 21:47:12 +0300

    Function for entries

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ fetch("https://api.frankfurter.app/currencies")
         display(data);
     });
 
+function display(data) {
+  const entries = Object.entries(data);
+  entries.forEach((entry) => {
+    select[0].innerHTML += `<option value="${entry[0]}">${entry[0]}</option>`;
+    select[1].innerHTML += `<option value="${entry[0]}">${entry[0]}</option>`;
+  });
+}
+
