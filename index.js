7c458d599074b9c8f4c1b457d0f1730a3036b6ad
Kevin <kevin.kiseli@student.moringaschool.com>
2023-11-03 21:41:47 +0300

    GET request

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ const num = document.getElementById("num");
 const answer = document.getElementById("answer");
 
 
+fetch("https://api.frankfurter.app/currencies")
+    .then((data) => data.json())
+    .then((data) => {
+        display(data);
+    });
+
