67ce00e43bca23e0123cc94881793b3832f3127c
Kevin <kevin.kiseli@student.moringaschool.com>
2023-11-03 21:38:05 +0300

    .js start

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 
+
+const select = document.querySelectorAll(".Currency");
+const btn = document. getElementById("btn");
+const num = document.getElementById("num");
+const answer = document.getElementById("answer");
+
+
