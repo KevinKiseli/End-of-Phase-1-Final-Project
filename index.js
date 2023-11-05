76651dc2347720b94d9612cfef4c2f53d5277e6b
Kevin <kevin.kiseli@student.moringaschool.com>
2023-11-05 21:06:01 +0300

    Final touches

--- index.js
+++ index.js
@@ -1,51 +1,54 @@
 
 
+// Selecting all elements with class "currency", id "num", id "ans", id "btn"
 const select = document.querySelectorAll(".currency");
-const btn = document.getElementById("btn");
 const num = document.getElementById("num");
 const ans = document.getElementById("ans");
+const btn = document.getElementById("btn");
 
+
+
+// Fetching and displaying the list of currencies from the API
 fetch("https://api.frankfurter.app/currencies")
   .then((data) => data.json())
   .then((data) => {
     display(data);
   });
 
+// Function to display the list of currencies in the dropdown menus both top and bottom menus
 function display(data) {
   const entries = Object.entries(data);
-  for (var i = 0; i < entries.length; i++) {
-    select[0].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
-    select[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
+  for (let i = 0; i < entries.length; i++) {
+       select[0].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
+       select[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
   }
 }
 
+// Adding an event listener to the button click event
 btn.addEventListener("click", () => {
-  let currency1 = select[0].value;
-  let currency2 = select[1].value;
+  // Getting the selected currencies and value from the input
+  let firstCurrency = select[0].value;
+  let secondCurrency = select[1].value;
   let value = num.value;
 
-  if (currency1 != currency2) {
-    convert(currency1, currency2, value);
+  // Checking if the selected currencies are not the same and calling the selected currencies values
+  if (firstCurrency != secondCurrency) {
+     convert(firstCurrency, secondCurrency, value);
   } else {
-    alert("Currency cannot be the same !");
+     alert("Currency cannot be the same !");
   }
 });
 
-function convert(currency1, currency2, value) {
+// Function to convert the currencies by fetching the rate from th API
+function convert(firstCurrency, secondCurrency, value) {
   const host = "api.frankfurter.app";
   fetch(
-    `https://${host}/latest?amount=${value}&from=${currency1}&to=${currency2}`
+    `https://${host}/latest?amount=${value}&from=${firstCurrency}&to=${secondCurrency}`
   )
     .then((val) => val.json())
     .then((val) => {
       console.log(Object.values(val.rates)[0]);
+      // Displaying the conversion rate in the output element
       ans.value = Object.values(val.rates)[0];
     });
-}
-    
-
-
-
-
-
-  
+}  
